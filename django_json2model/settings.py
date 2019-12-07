9d2c1801e203d711fe416b22c726fc28f6843d05
Grusinator <grusinator@gmail.com>
2019-12-07 16:46:21 +0100

    refactored views.py and models.py out to an example app

--- django_json2model/settings.py
+++ django_json2model/settings.py
@@ -84,13 +84,6 @@ WSGI_APPLICATION = 'django_json2model.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
