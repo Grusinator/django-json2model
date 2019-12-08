238d41aa46b06342a2f4777d3c2ede60b0cc1c93
Grusinator <grusinator@gmail.com>
2019-12-08 08:58:33 +0100

    refactored admin handler

--- json2model/services/dynamic_model/dynamic_model_admin_handler.py
+++ json2model/services/dynamic_model/dynamic_model_admin_handler.py
@@ -2,7 +2,6 @@ import logging
 from importlib import import_module, reload
 
 from django.contrib import admin
-from django.contrib.admin.sites import NotRegistered
 from django.urls import clear_url_caches
 from mutant.models import ModelDefinition
 
@@ -15,19 +14,19 @@ APP_LABEL = settings.APP_LABEL_DYNAMIC_MODELS
 def register_all_models():
     model_defs = ModelDefinition.objects.filter(app_label=APP_LABEL)
     for model_def in model_defs:
-        try:
-            register_model_in_admin(model_def)
-        except admin.sites.AlreadyRegistered:
-            pass
-    reload_and_clear_cache_admin()
+        try_register_model_in_admin(model_def)
 
 
-def register_model_in_admin(model_def):
+def try_register_model_in_admin(model_def):
     ObjectModel = model_def.model_class()
     attrs = {'model': ObjectModel}
     ObjectModelAdmin = type(f'{ObjectModel.__name__}Admin', (admin.ModelAdmin,), attrs)
-    admin.site.register(ObjectModel, ObjectModelAdmin)
-    reload_and_clear_cache_admin()
+    try:
+        admin.site.register(ObjectModel, ObjectModelAdmin)
+        reload_and_clear_cache_admin()
+    except admin.sites.AlreadyRegistered:
+        logger.warning(f"model_def: {model_def.name}, was already registered in admin.site, "
+                       f"and could therefore not register")
 
 
 def try_unregister_model_in_admin(model_def):
@@ -35,7 +34,7 @@ def try_unregister_model_in_admin(model_def):
     try:
         admin.site.unregister(ObjectModel)
         reload_and_clear_cache_admin()
-    except NotRegistered:
+    except admin.sites.NotRegistered:
         logger.warning(f"model_def: {model_def.name}, was not registered in admin.site, "
                        f"and could therefore not unregister")
 
