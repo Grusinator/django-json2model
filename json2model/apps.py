082707c6fbd3f45ac33fdc423d30a1b9b4e5425e
Grusinator <grusinator@gmail.com>
2019-12-08 11:17:34 +0100

    add create all models in admin, from data

--- json2model/apps.py
+++ json2model/apps.py
@@ -4,4 +4,6 @@ from django.apps import AppConfig
 class Json2ModelConfig(AppConfig):
     name = 'json2model'
 
-
+    def ready(self):
+        import json2model.services.dynamic_model.dynamic_model_admin_handler as admin_handler
+        admin_handler.register_all_models()
